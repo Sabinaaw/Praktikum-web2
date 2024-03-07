25f3159cf09cb2a5138270a1534368a0b23fa963
Sabinaaw <sabrina.jrd.06@gmail.com>
2024-03-07 20:51:22 +0700

    praktikum 2

---
+++ praktikum1/variabel.php
@@ -0,0 +1,13 @@
+<?php
+  // echo "Hello World!";
+
+  // Membuat Variabel
+  $nama = "Sabrina Jasmine Rambu Diena";
+  $jurusan = "Teknik Informatika";
+  $angkatan = 2023;
+
+
+  // output
+  echo "Nama saya adalah : " . $nama;
+    
+
---
+++ praktikum1/variabel1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contoh</title>
+</head>
+<body>
+  <h2><?php echo $nama ?></h2>
+  <p>
+      saya berada di angkatan <?= $angkatan?>
+  </p>
+    
+</body>
+</html>
